1c809dc39ccf12d7a4e5350eff38971bc79f3d53
HYEONGYUN0823 <shiningin00@naver.com>
2025-04-28 16:26:10 +0900

    khy

--- bada/src/main/java/com/a7a7/module/codegroup/CodeGroupController.java
+++ bada/src/main/java/com/a7a7/module/codegroup/CodeGroupController.java
@@ -1,8 +1,16 @@
 package com.a7a7.module.codegroup;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CodeGroupController {
-
+	
+	@RequestMapping(value = "/index")
+	public String requestMethodName() {
+		
+		return "/usr/index";
+	}
+	
+	
 }
