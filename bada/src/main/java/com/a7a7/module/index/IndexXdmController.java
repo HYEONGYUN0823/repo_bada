4412f25a503738950c1ee7dec22976c9ea7d4da6
YONGYONGB <aa39421371@gmail.com>
2025-04-29 14:51:15 +0900

    kyy

---
+++ bada/src/main/java/com/a7a7/module/index/IndexXdmController.java
@@ -0,0 +1,13 @@
+package com.a7a7.module.index;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexXdmController {
+
+	@RequestMapping(value="/xdm/")
+	public String indexXdm() {
+		return "xdm/index/index";
+	}
+}
