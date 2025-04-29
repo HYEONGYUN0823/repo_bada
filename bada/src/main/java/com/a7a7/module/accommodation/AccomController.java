a181fd8513ac791ae14bf829002fd1a042fe33bc
tdj4909 <tdj4909@gmail.com>
2025-04-29 17:48:49 +0900

    ojw

--- bada/src/main/java/com/a7a7/module/accommodation/AccomController.java
+++ bada/src/main/java/com/a7a7/module/accommodation/AccomController.java
@@ -1,13 +1,14 @@
 package com.a7a7.module.accommodation;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AccomController {
 
 	@RequestMapping("/xdm/accom/list")
-	public String getAccomList() {
+	public String getAccomList(Model model) {
 		
 		return "xdm/accom/accomList";
 	}
