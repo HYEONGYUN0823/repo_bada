a0f216082d44cdf149e972073fcb39f139b34e02
YONGYONGB <aa39421371@gmail.com>
2025-04-29 17:45:18 +0900

    kyy

--- bada/src/main/java/com/a7a7/module/sea/SeaController.java
+++ bada/src/main/java/com/a7a7/module/sea/SeaController.java
@@ -19,17 +19,8 @@ public class SeaController {
 	
 	
 	@RequestMapping(value="xdm/travel")
-	public String travel() {
-		return "/xdm/travel/travel";
-	}
-	
-	
-	
-	
-	@RequestMapping(value = "/travelJsonNodeList")
-	public String travelJsonNodeList(Model model) throws Exception {
-		
-		String apiUrl = "https://apis.data.go.kr/1192136/fcstSeaTrip/GetFcstSeaTripApiService?serviceKey=9GyQfz7WfYE5IWKlIQB2FaXDXc2h8lfc%2Fy871UUm%2BNBS1fO5SbIUk%2FeVUdFyctG2PfyrWJAhHCyeu6H5pYr1KQ%3D%3D&type=json&reqDate=&pageNo=1&numOfRows=300&include=lastScr,sareaDtlNm,lat,lot,predcYmd,predcNoonSeCd,avgArtmp,avgWspd,avgWtem,avgWvhgt,avgCrsp,weather,totalIndex";
+	public String travel(Model model) throws Exception {
+		String apiUrl = "";
 		URL url = new URL(apiUrl);
 		HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection(); //openConnection() : 해당 URL로 연결을 여는 거야.
 		httpURLConnection.setRequestMethod("GET"); //HttpURLConnection : HTTP 요청을 보낼 수 있는 클래스야., setRequestMethod("GET") : GET 방식으로 요청을 보낸다는 의미.
@@ -54,25 +45,7 @@ public class SeaController {
 		ObjectMapper objectMapper = new ObjectMapper();			//ObjectMapper는 Jackson 라이브러리에서 제공하는 JSON 처리용 클래스야.
 		JsonNode node = objectMapper.readTree(stringBuilder.toString()); //JsonNode node = objectMapper.readTree(stringBuilder.toString());
 		
-		System.out.println("node.get(\"header\").get(\"resultCode\").asText(): " + node.get("header").get("resultCode").asText());
-		System.out.println("node.get(\"header\").get(\"resultMsg\").asText(): " + node.get("header").get("resultMsg").asText());
-		System.out.println("node.get(\"header\").get(\"resultMsg\").asText(): " + node.get("body").get("items").get(1).get("lastScr").asText());
-		System.out.println("node.get(\"header\").get(\"resultMsg\").asText(): " + node.get("body").get("items").get(1).get("sareaDtlNm").asText());
-		System.out.println("node.get(\"header\").get(\"resultMsg\").asText(): " + node.get("body").get("items").get(1).get("lat").asText());
-		System.out.println("node.get(\"header\").get(\"resultMsg\").asText(): " + node.get("body").get("items").get(1).get("lot").asText());
-		System.out.println("node.get(\"header\").get(\"resultMsg\").asText(): " + node.get("body").get("items").get(1).get("predcYmd").asText());
-		System.out.println("node.get(\"header\").get(\"resultMsg\").asText(): " + node.get("body").get("items").get(1).get("predcNoonSeCd").asText());
-		System.out.println("node.get(\"header\").get(\"resultMsg\").asText(): " + node.get("body").get("items").get(1).get("avgArtmp").asText());
-		System.out.println("node.get(\"header\").get(\"resultMsg\").asText(): " + node.get("body").get("items").get(1).get("avgWspd").asText());
-		System.out.println("node.get(\"header\").get(\"resultMsg\").asText(): " + node.get("body").get("items").get(1).get("avgWtem").asText());
-		System.out.println("node.get(\"header\").get(\"resultMsg\").asText(): " + node.get("body").get("items").get(1).get("avgWvhgt").asText());
-		System.out.println("node.get(\"header\").get(\"resultMsg\").asText(): " + node.get("body").get("items").get(1).get("avgCrsp").asText());
-		System.out.println("node.get(\"header\").get(\"resultMsg\").asText(): " + node.get("body").get("items").get(1).get("weather").asText());
-		System.out.println("node.get(\"header\").get(\"resultMsg\").asText(): " + node.get("body").get("items").get(1).get("totalIndex").asText());
-		
-		model.addAttribute("node", node);
-		
-		
+		model.addAttribute("items", node.toString());
 		return "/xdm/travel/travel";
 	}
 	
