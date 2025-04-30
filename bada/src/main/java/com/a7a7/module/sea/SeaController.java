d95349049564862036ea6eeeb2ddf2c34851a616
YONGYONGB <aa39421371@gmail.com>
2025-04-30 15:14:51 +0900

    kyy

--- bada/src/main/java/com/a7a7/module/sea/SeaController.java
+++ bada/src/main/java/com/a7a7/module/sea/SeaController.java
@@ -4,8 +4,10 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +18,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Controller
 public class SeaController {
 
-	
+	@Value("${sea_api}")
+	private String seaApiUrl;
 	
 	@RequestMapping(value="xdm/travel")
-	public String travel(Model model) throws Exception {
-		String apiUrl = "";
-		URL url = new URL(apiUrl);
+	public String travel(Model model) throws Exception {;
+		URL url = new URL(seaApiUrl);
 		HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection(); //openConnection() : 해당 URL로 연결을 여는 거야.
 		httpURLConnection.setRequestMethod("GET"); //HttpURLConnection : HTTP 요청을 보낼 수 있는 클래스야., setRequestMethod("GET") : GET 방식으로 요청을 보낸다는 의미.
 		
@@ -45,9 +47,29 @@ public class SeaController {
 		ObjectMapper objectMapper = new ObjectMapper();			//ObjectMapper는 Jackson 라이브러리에서 제공하는 JSON 처리용 클래스야.
 		JsonNode node = objectMapper.readTree(stringBuilder.toString()); //JsonNode node = objectMapper.readTree(stringBuilder.toString());
 		
-		JsonNode itemsNode = node.path("response").path("body").path("items").path("item"); 
-		//이렇게 중첩된 구조에서 items는 객체이고, 그 안에 item 배열이 존재하기 때문에 items.get("item")으로 배열을 꺼내서 넘겨줘야 Thymeleaf에서 이를 순회하며 값을 출력할 수 있게 됩니다.
-		model.addAttribute("items", itemsNode);
+		JsonNode itemsNode = node.path("response").path("body").path("items").path("item");  //이렇게 중첩된 구조에서 items는 객체이고, 그 안에 item 배열이 존재하기 때문에 items.get("item")으로 배열을 꺼내서 넘겨줘야 Thymeleaf에서 이를 순회하며 값을 출력할 수 있게 됩니다.
+		
+		List<SeaDto> itemList = new ArrayList<>(); //items 데이터가 JSON 배열 형식이라면, 이를 제대로 List 형태로 변환하여 모델에 전달하는 것이 좋습니다. JsonNode에서 직접 데이터를 추출할 때, JsonNode를 List나 DTO 객체로 변환하는 것이 중요
+	    if (itemsNode.isArray()) {
+	        for (JsonNode itemNode : itemsNode) {
+	        	SeaDto itemDTO = new SeaDto();
+	            itemDTO.setLastScr(itemNode.path("lastScr").asText());
+	            itemDTO.setSareaDtlNm(itemNode.path("sareaDtlNm").asText());
+	            itemDTO.setLat(itemNode.path("lat").asText());
+	            itemDTO.setLot(itemNode.path("lot").asText());
+	            itemDTO.setPredcYmd(itemNode.path("predcYmd").asText());
+	            itemDTO.setPredcNoonSeCd(itemNode.path("predcNoonSeCd").asText());
+	            itemDTO.setAvgArtmp(itemNode.path("avgArtmp").asText());
+	            itemDTO.setAvgWspd(itemNode.path("avgWspd").asText());
+	            itemDTO.setAvgWtem(itemNode.path("avgWtem").asText());
+	            itemDTO.setAvgWvhgt(itemNode.path("avgWvhgt").asText());
+	            itemDTO.setAvgCrsp(itemNode.path("avgCrsp").asText());
+	            itemDTO.setWeather(itemNode.path("weather").asText());
+	            itemDTO.setTotalIndex(itemNode.path("totalIndex").asText());          
+	            itemList.add(itemDTO);
+	        }
+	    }
+		model.addAttribute("items", itemList);
 		return "/xdm/travel/travel";
 	}
 	
---
+++ bada/src/main/java/com/a7a7/module/sea/SeaDto.java
@@ -0,0 +1,102 @@
+package com.a7a7.module.sea;
+
+public class SeaDto {
+	private String  lastScr;
+    private String sareaDtlNm;
+    private String  lat;
+    private String  lot;
+    private String predcYmd;
+    private String predcNoonSeCd;
+    private String avgArtmp;
+    private String avgWspd;
+    private String avgWtem;
+    private String avgWvhgt;
+    private String avgCrsp;
+    private String weather;
+    private String totalIndex;
+    
+    
+    
+	public String getLastScr() {
+		return lastScr;
+	}
+	public void setLastScr(String lastScr) {
+		this.lastScr = lastScr;
+	}
+	public String getSareaDtlNm() {
+		return sareaDtlNm;
+	}
+	public void setSareaDtlNm(String sareaDtlNm) {
+		this.sareaDtlNm = sareaDtlNm;
+	}
+	public String getLat() {
+		return lat;
+	}
+	public void setLat(String lat) {
+		this.lat = lat;
+	}
+	public String getLot() {
+		return lot;
+	}
+	public void setLot(String lot) {
+		this.lot = lot;
+	}
+	public String getPredcYmd() {
+		return predcYmd;
+	}
+	public void setPredcYmd(String predcYmd) {
+		this.predcYmd = predcYmd;
+	}
+	public String getPredcNoonSeCd() {
+		return predcNoonSeCd;
+	}
+	public void setPredcNoonSeCd(String predcNoonSeCd) {
+		this.predcNoonSeCd = predcNoonSeCd;
+	}
+	public String getAvgArtmp() {
+		return avgArtmp;
+	}
+	public void setAvgArtmp(String avgArtmp) {
+		this.avgArtmp = avgArtmp;
+	}
+	public String getAvgWspd() {
+		return avgWspd;
+	}
+	public void setAvgWspd(String avgWspd) {
+		this.avgWspd = avgWspd;
+	}
+	public String getAvgWtem() {
+		return avgWtem;
+	}
+	public void setAvgWtem(String avgWtem) {
+		this.avgWtem = avgWtem;
+	}
+	public String getAvgWvhgt() {
+		return avgWvhgt;
+	}
+	public void setAvgWvhgt(String avgWvhgt) {
+		this.avgWvhgt = avgWvhgt;
+	}
+	public String getAvgCrsp() {
+		return avgCrsp;
+	}
+	public void setAvgCrsp(String avgCrsp) {
+		this.avgCrsp = avgCrsp;
+	}
+	public String getWeather() {
+		return weather;
+	}
+	public void setWeather(String weather) {
+		this.weather = weather;
+	}
+	public String getTotalIndex() {
+		return totalIndex;
+	}
+	public void setTotalIndex(String totalIndex) {
+		this.totalIndex = totalIndex;
+	}
+	
+    
+    
+    
+}
