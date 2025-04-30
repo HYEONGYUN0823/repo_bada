8c67e1fbf8cf56435e1871bdceb141b22a5b3df3
tdj4909 <tdj4909@gmail.com>
2025-04-30 16:07:15 +0900

    ojw

--- bada/src/main/java/com/a7a7/module/accom/AccomController.java
+++ bada/src/main/java/com/a7a7/module/accom/AccomController.java
@@ -1,14 +1,53 @@
 package com.a7a7.module.accom;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Controller
 public class AccomController {
-
+	
+	@Value("${accom_api_key}")
+	private String serviceKey;
+	
 	@RequestMapping("/xdm/accom/list")
-	public String getAccomList(Model model) {
+	public String getAccomList(Model model) throws Exception {
+		
+		String apiUrl = "http://apis.data.go.kr/B551011/KorService1/searchStay1?areaCode=32&sigunguCode=1&listYN=Y&MobileOS=ETC&MobileApp=AppTest&arrange=A&numOfRows=12&pageNo=1&_type=json&ServiceKey=" + serviceKey;
+		
+		URL url = new URL(apiUrl);
+		HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+		httpURLConnection.setRequestMethod("GET");
+		
+		BufferedReader bufferedReader;
+		if (httpURLConnection.getResponseCode() >= 200 && httpURLConnection.getResponseCode() <= 300) {
+			bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+		} else {
+			bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getErrorStream()));
+		}
+		
+		StringBuilder stringBuilder = new StringBuilder();
+		String line;
+		while ((line = bufferedReader.readLine()) != null) {
+			System.out.println("line: " + line);
+			stringBuilder.append(line);
+		}
+
+		bufferedReader.close();
+		httpURLConnection.disconnect();
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		AccomDto dto = objectMapper.readValue(stringBuilder.toString(), AccomDto.class);
+		
+		model.addAttribute("list", dto.getResponse().getBody().getItems().getItem());
 		
 		return "xdm/accom/accomList";
 	}
---
+++ bada/src/main/java/com/a7a7/module/accom/AccomDto.java
@@ -0,0 +1,166 @@
+package com.a7a7.module.accom;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+public class AccomDto {
+	private Response response;
+
+	public static class Response {
+		private Header header;
+		private Body body;		
+		
+		public static class Header {
+			private String resultCode;
+			private String resultMsg;
+//			Getter, Setter			
+			public String getResultCode() {
+				return resultCode;
+			}
+			public void setResultCode(String resultCode) {
+				this.resultCode = resultCode;
+			}
+			public String getResultMsg() {
+				return resultMsg;
+			}
+			public void setResultMsg(String resultMsg) {
+				this.resultMsg = resultMsg;
+			}
+//			Constructor
+			public Header() {
+				super();
+			}
+		}
+		public static class Body {
+			private Items items;
+			private String numOfRows;
+			private String pageNo;
+			private String totalCount;
+			
+			public static class Items {
+				private List<Item> item;
+				
+				@JsonIgnoreProperties(ignoreUnknown = true)
+				public static class Item {
+					private String title;
+					private String createdtime;
+					private String modifiedtime;
+					private String tel;
+					private String addr1;
+					private String mapx;
+					private String mapy;
+//					Constructor
+					public Item() {}	
+//					Getter, Setter
+					public String getTitle() {
+						return title;
+					}
+					public void setTitle(String title) {
+						this.title = title;
+					}
+					public String getCreatedtime() {
+						return createdtime;
+					}
+					public void setCreatedtime(String createdtime) {
+						this.createdtime = createdtime;
+					}
+					public String getModifiedtime() {
+						return modifiedtime;
+					}
+					public void setModifiedtime(String modifiedtime) {
+						this.modifiedtime = modifiedtime;
+					}
+					public String getTel() {
+						return tel;
+					}
+					public void setTel(String tel) {
+						this.tel = tel;
+					}
+					public String getAddr1() {
+						return addr1;
+					}
+					public void setAddr1(String addr1) {
+						this.addr1 = addr1;
+					}
+					public String getMapx() {
+						return mapx;
+					}
+					public void setMapx(String mapx) {
+						this.mapx = mapx;
+					}
+					public String getMapy() {
+						return mapy;
+					}
+					public void setMapy(String mapy) {
+						this.mapy = mapy;
+					}
+			
+				}
+//				Getter, Setter
+				public List<Item> getItem() {
+					return item;
+				}
+				public void setItem(List<Item> item) {
+					this.item = item;
+				}
+				
+//				Constructor
+				public Items() {}
+			}
+//			Getter, Setter
+			public Items getItems() {
+				return items;
+			}
+			public void setItems(Items items) {
+				this.items = items;
+			}
+			public String getNumOfRows() {
+				return numOfRows;
+			}
+			public void setNumOfRows(String numOfRows) {
+				this.numOfRows = numOfRows;
+			}
+			public String getPageNo() {
+				return pageNo;
+			}
+			public void setPageNo(String pageNo) {
+				this.pageNo = pageNo;
+			}
+			public String getTotalCount() {
+				return totalCount;
+			}
+			public void setTotalCount(String totalCount) {
+				this.totalCount = totalCount;
+			}
+//			Constructor
+			public Body() {}
+		}
+//		Getter, Setter	
+		public Header getHeader() {
+			return header;
+		}
+		public void setHeader(Header header) {
+			this.header = header;
+		}
+		public Body getBody() {
+			return body;
+		}
+		public void setBody(Body body) {
+			this.body = body;
+		}
+//		Constructor
+		public Response() {
+			super();
+		}
+	}
+//	Constructor
+	public AccomDto() {}
+//	Getter, Setter
+	public Response getResponse() {
+		return response;
+	}
+	public void setResponse(Response response) {
+		this.response = response;
+	}
+}
