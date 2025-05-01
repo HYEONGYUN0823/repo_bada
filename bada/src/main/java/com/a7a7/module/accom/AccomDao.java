f62f0131e248112c9b94f0846648afa5c01ebbb6
tdj4909 <tdj4909@gmail.com>
2025-05-01 14:33:46 +0900

    ojw

---
+++ bada/src/main/java/com/a7a7/module/accom/AccomDao.java
@@ -0,0 +1,8 @@
+package com.a7a7.module.accom;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccomDao {
+
+}
---
+++ bada/src/main/java/com/a7a7/module/accom/AccomDto.java
@@ -0,0 +1,83 @@
+package com.a7a7.module.accom;
+
+public class AccomDto {
+	private String accommodation_id;
+	private String title;
+	private String overview;
+	private Integer capacity;
+	private String tel;
+	private String address;
+	private String mapx;
+	private String mapy;
+	private String createdtime;
+	private String modifiedtime;
+	private String manager_id;
+	
+	
+	public String getAccommodation_id() {
+		return accommodation_id;
+	}
+	public void setAccommodation_id(String accommodation_id) {
+		this.accommodation_id = accommodation_id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getOverview() {
+		return overview;
+	}
+	public void setOverview(String overview) {
+		this.overview = overview;
+	}
+	public Integer getCapacity() {
+		return capacity;
+	}
+	public void setCapacity(Integer capacity) {
+		this.capacity = capacity;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getMapx() {
+		return mapx;
+	}
+	public void setMapx(String mapx) {
+		this.mapx = mapx;
+	}
+	public String getMapy() {
+		return mapy;
+	}
+	public void setMapy(String mapy) {
+		this.mapy = mapy;
+	}
+	public String getCreatedtime() {
+		return createdtime;
+	}
+	public void setCreatedtime(String createdtime) {
+		this.createdtime = createdtime;
+	}
+	public String getModifiedtime() {
+		return modifiedtime;
+	}
+	public void setModifiedtime(String modifiedtime) {
+		this.modifiedtime = modifiedtime;
+	}
+	public String getManager_id() {
+		return manager_id;
+	}
+	public void setManager_id(String manager_id) {
+		this.manager_id = manager_id;
+	}
+}
