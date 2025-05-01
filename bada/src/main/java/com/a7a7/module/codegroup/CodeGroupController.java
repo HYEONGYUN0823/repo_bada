f26929343651ffbcb410c8df63a996d49058c11a
HYEONGYUN0823 <shiningin00@naver.com>
2025-05-01 16:11:16 +0900

    khy

--- bada/src/main/java/com/a7a7/module/codegroup/CodeGroupController.java
+++ bada/src/main/java/com/a7a7/module/codegroup/CodeGroupController.java
@@ -5,57 +5,5 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CodeGroupController {
-	
-	@RequestMapping(value = "/index")
-	public String index() {
-		
-		return "usr/index/index";
-	}
-	
-//	---------------------------------
-//	              여행지
-//	---------------------------------
-	
-	@RequestMapping(value = "/travelList")
-	public String travelList() {
-		
-		return "usr/list/travelList";
-	}
-	
-	@RequestMapping(value = "/travelView")
-	public String travelView() {
-		
-		return "usr/view/travelView";
-	}
-	
-//	---------------------------------
-//                식당
-//  ---------------------------------
-	
-	@RequestMapping(value = "/restaurantList")
-	public String restaurantList() {
-		
-		return "usr/list/restaurantList";
-	}	
-	
-//	---------------------------------
-//                숙박업소
-//  ---------------------------------
-	
-	@RequestMapping(value = "/hotelList")
-	public String hotelList() {
-		
-		return "usr/list/hotelList";
-	}	
-	
-//	---------------------------------
-//                1:1문의
-//  ---------------------------------
-	
-	@RequestMapping(value = "/contact")
-	public String contact() {
-		
-		return "usr/contact/contact";
-	}	
-	
+
 }
---
+++ bada/src/main/java/com/a7a7/module/userUi/UserUiController.java
@@ -0,0 +1,73 @@
+package com.a7a7.module.userUi;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserUiController {
+	
+	@RequestMapping(value = "/index")
+	public String index() {
+		
+		return "usr/index/index";
+	}
+	
+//	---------------------------------
+//	              여행지
+//	---------------------------------
+	
+	@RequestMapping(value = "/travelList")
+	public String travelList() {
+		
+		return "usr/travel/travelList";
+	}
+	
+	@RequestMapping(value = "/travelView")
+	public String travelView() {
+		
+		return "usr/travel/travelView";
+	}
+	
+//	---------------------------------
+//                식당
+//  ---------------------------------
+	
+	@RequestMapping(value = "/restaurantList")
+	public String restaurantList() {
+		
+		return "usr/restaurant/restaurantList";
+	}	
+	
+//	---------------------------------
+//                숙박업소
+//  ---------------------------------
+	
+	@RequestMapping(value = "/accomList")
+	public String accomList() {
+		
+		return "usr/accom/accomList";
+	}	
+	
+//	---------------------------------
+//                1:1문의
+//  ---------------------------------
+	
+	@RequestMapping(value = "/contact")
+	public String contact() {
+		
+		return "usr/contact/contact";
+	}	
+	
+	@RequestMapping(value = "/test")
+	public String test() {
+		
+		return "usr/test";
+	}
+	
+	@RequestMapping(value = "/checkout")
+	public String checkout() {
+		
+		return "usr/checkout/checkout";
+	}
+	
+}
