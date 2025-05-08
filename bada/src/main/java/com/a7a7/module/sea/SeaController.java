fe5fd1c15a4661daec17bdaac216b2a7f7dc277d
YONGYONGB <aa39421371@gmail.com>
2025-05-08 18:09:15 +0900

    kyy

--- bada/src/main/java/com/a7a7/module/sea/SeaController.java
+++ bada/src/main/java/com/a7a7/module/sea/SeaController.java
@@ -1,18 +1,24 @@
 package com.a7a7.module.sea;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.a7a7.module.common.PageVo;
 
 @Controller
 public class SeaController {
 	
 	@Autowired
 	SeaService service;
+	
+	@Value("${kakao_map_api}")
+	private String kakaoApiKey;
 
 	 
 	//******************//
@@ -22,8 +28,8 @@ public class SeaController {
 	////////// 바다여행지수///////////////
 
 	@RequestMapping(value="xdm/travel")
-	public String Travel(Model model) throws Exception {
-		service.setlist();
+	public String Travel(PageVo pageVo,Model model) throws Exception {
+		service.setlist(pageVo);
 		model.addAttribute("items", service.forecastList());
 		return "/xdm/travel/travel";
 	}
@@ -31,9 +37,9 @@ public class SeaController {
 	
 	//////////바다여행장소///////////////
 	@RequestMapping(value="xdm/seaTravel")
-	public String seaTravel(Model model)throws Exception{
-		service.setlist();
-		model.addAttribute("items",service.seaList());
+	public String seaTravel(PageVo pageVo,Model model)throws Exception{
+		service.setlist(pageVo);
+		model.addAttribute("items",service.seaList(pageVo));
 		return "/xdm/travel/seaTravel";
 	}
 	
@@ -42,9 +48,21 @@ public class SeaController {
 	//******************//
 	
 	@GetMapping("/bada/travel/list")
-	public String findUsrAccomList(Model model) {	
+	public String findUsrTravelList(@RequestParam(name = "page", defaultValue = "1") int page ,Model model) {	
+		// 페이징
+		PageVo pageVo = new PageVo();
+		pageVo.setThisPage(page);
+		pageVo.setParamsPaging(service.countSeaList());
+		model.addAttribute("pageVo", pageVo);
 		// DB에서 숙박업소 전체 출력
-		model.addAttribute("list", service.seaList());
+		model.addAttribute("list", service.seaList(pageVo));
 		return "usr/travel/travelList";
+	}
+	
+	@GetMapping("/bada/travel/{id}")
+	public String findUsrTravelView(@PathVariable("id") String sea_id,Model model){
+		model.addAttribute("list",service.seaView(sea_id));
+		model.addAttribute("kakaoApiKey", kakaoApiKey);
+		return "usr/travel/travelView";
 	}
 }
--- bada/src/main/java/com/a7a7/module/sea/SeaDao.java
+++ bada/src/main/java/com/a7a7/module/sea/SeaDao.java
@@ -4,12 +4,20 @@ import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
+import com.a7a7.module.common.PageVo;
+
 @Repository
 public interface SeaDao {
 
-	public List<SeaDto> seaList();
-	public List<SeaDto> forecastList();
+	public List<SeaDto> seaList(PageVo pageVo);		// 바다여행지역 리스트
+	public List<SeaDto> forecastList(); // 바다여행지역의 바다여행지수 리스트
 	
-	public int seaInsert(SeaDto dto);
-	public int forecastInsert(SeaDto dto);
+	public int seaInsert(SeaDto dto); // api에서 바다여행지역 리스트
+	public int forecastInsert(SeaDto dto); // api에서 바다여행지수 리스트
+	
+	
+	public int countSeaList(); // 바다여행지역 전체 개수
+	
+	//사용자부분
+	public SeaDto seaView(String sea_id);
 }
