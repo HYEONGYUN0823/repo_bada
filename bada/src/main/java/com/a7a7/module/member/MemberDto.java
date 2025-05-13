3e7322ffc822d8b207b2f1d60e4fc2d88e40fdcc
tdj4909 <tdj4909@gmail.com>
2025-05-13 13:01:19 +0900

    ojw

--- bada/src/main/java/com/a7a7/module/member/MemberDto.java
+++ bada/src/main/java/com/a7a7/module/member/MemberDto.java
@@ -2,6 +2,55 @@ package com.a7a7.module.member;
 
 public class MemberDto {
 	
+	private String memberId;
+	private String email;
+	private String password;
+	private String name;
+	private String createdtime;
+	private String modifiedtime;
+	private Integer delNy;
 	
 	
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCreatedtime() {
+		return createdtime;
+	}
+	public void setCreatedtime(String createdtime) {
+		this.createdtime = createdtime;
+	}
+	public String getModifiedtime() {
+		return modifiedtime;
+	}
+	public void setModifiedtime(String modifiedtime) {
+		this.modifiedtime = modifiedtime;
+	}
+	public Integer getDelNy() {
+		return delNy;
+	}
+	public void setDelNy(Integer delNy) {
+		this.delNy = delNy;
+	}
 }
