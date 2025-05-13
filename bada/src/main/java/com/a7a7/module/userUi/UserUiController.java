b1ec8cd4a76d25a94c7554e2f9923a97f317f52e
YONGYONGB <aa39421371@gmail.com>
2025-05-13 12:08:39 +0900

    kyy

--- bada/src/main/java/com/a7a7/module/userUi/UserUiController.java
+++ bada/src/main/java/com/a7a7/module/userUi/UserUiController.java
@@ -1,9 +1,12 @@
 package com.a7a7.module.userUi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.a7a7.module.sea.SeaService;
 
 @Controller
 public class UserUiController {
@@ -11,8 +14,13 @@ public class UserUiController {
 	@Value("${kakao_map_api}")
 	private String kakaoApiKey;
 	
+	@Autowired
+	SeaService seaService;	//홈페이지화면에서 DB에 있는 여행지 위도경도값 받기 위해서 불러옴.
+	
+	
 	@RequestMapping(value = "/index")
 	public String index(Model model) {
+		model.addAttribute("items", seaService.seaList()); //여행지 위도,경도 값 자바스크립트에 보내기.
 		model.addAttribute("kakaoApiKey", kakaoApiKey);
 		return "usr/index/index";
 	}
