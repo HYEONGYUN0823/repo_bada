4ae85db6ffad1bce83bb186a796a91a4e5de4525
HYEONGYUN0823 <shiningin00@naver.com>
2025-05-19 17:50:01 +0900

    khy

--- bada/src/main/java/com/a7a7/module/payment/PaymentController.java
+++ bada/src/main/java/com/a7a7/module/payment/PaymentController.java
@@ -1,14 +1,24 @@
 package com.a7a7.module.payment;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.a7a7.module.accom.AccomService;
+import com.a7a7.module.restaurant.RestaurantService;
+import com.a7a7.module.sea.SeaService;
 
 @Controller
 public class PaymentController {
-	
+	@Autowired
+	SeaService seaService;
+	@Autowired
+	RestaurantService restaurantService;
+	@Autowired
+	AccomService accomService;
 	
 //    @Value("${toss.payments.secret-key}")
 //    private String secretKey;
@@ -40,4 +50,38 @@ public class PaymentController {
 //    public String paymentFail(@RequestParam String code, @RequestParam String message) {
 //        return "결제 실패: " + message + " (코드: " + code + ")";
 //    }
+	
+	@RequestMapping(value = "/paymentSuccess")
+	public String paymentSuccess() {
+		return "usr/payment/paymentSuccess";
+	}
+	
+	/* ================================
+	 				실패
+	=================================== */
+	
+	@GetMapping("/fail/{type}/{id}")
+	public String paymentFailPage(@PathVariable String type, @PathVariable String id, Model model) {
+	    model.addAttribute("type", type);
+	    model.addAttribute("id", id);
+	    
+	    return "usr/payment/paymentFail";  // 결제 실패 알림 페이지 (html)
+	}
+	
+	@GetMapping("/fail/restaurant/{id}")
+	public String paymentFailRestaurant(@PathVariable("id") String restaurantId) {
+	    // 다시 해당 식당 상세페이지로 리다이렉트
+	    return "redirect:/fail/restaurant/" + restaurantId;
+	}
+
+	@GetMapping("/fail/accom/{id}")
+	public String paymentFailAccom(@PathVariable("id") String accomId) {
+	    return "redirect:/bada/accom/" + accomId;
+	}
+
+	@GetMapping("/fail/travel/{id}")
+	public String paymentFailTravel(@PathVariable("id") String sea_id) {
+	    return "redirect:/bada/travel/" + sea_id;
+	}
+	
 }
