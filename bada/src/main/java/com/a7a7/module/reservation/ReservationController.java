86053c530ef3a834335fc48b5bb65cf0e0683823
YONGYONGB <aa39421371@gmail.com>
2025-05-21 16:27:46 +0900

    kyy

--- bada/src/main/java/com/a7a7/module/reservation/ReservationController.java
+++ bada/src/main/java/com/a7a7/module/reservation/ReservationController.java
@@ -4,19 +4,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import com.a7a7.module.codegroup.CodeGroupController;
 import com.a7a7.module.common.PageVo;
 
 @Controller
 public class ReservationController {
 
+    private final CodeGroupController codeGroupController;
+
 	@Autowired
 	ReservationService service;
+
+    ReservationController(CodeGroupController codeGroupController) {
+        this.codeGroupController = codeGroupController;
+    }
 	
 	@RequestMapping(value = "/reservation")
 	public String order(PageVo pageVo,Model model){
+		int c = service.countReservationList(pageVo);
 		pageVo.setParamsPaging(service.countReservationList(pageVo));
 		model.addAttribute("pageVo", pageVo);
+		model.addAttribute("count", c);
 		model.addAttribute("items", service.reservationList(pageVo));
 		return "usr/information/reservation";
 	}
